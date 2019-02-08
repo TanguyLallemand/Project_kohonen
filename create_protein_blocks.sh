7bec479c3d29470aa23d38e1e42ee1a21f4d07f3
tanguy.lallemand <tanguy.lallemand@gmail.com>
2019-02-08 12:17:06 +0100

    optimised

--- create_protein_blocks.sh
+++ create_protein_blocks.sh
@@ -5,8 +5,7 @@ for i in `ls *.ang`; do
     name=$(basename $i)
     name=$(echo "$name" | cut -f 1 -d '.')
     # Get number of commas to determine how many protein blocks we can do
-    number_of_commas=`cat $name.ang| tr -cd , | wc -c`
-    j=$((number_of_commas / 2))
+    number_of_commas=`head -n1 $name.ang| tr -cd , | wc -c`
     # Change five by three because the two last Amino acids are missing with five. For example, for protein 1A3C, with j-5 last Amino acids are I, Y and E, N are missing. If we correct with 3 last amino acids are E and N.
     j=$((j-3))
     for((num=1;num<=$j;num++));do
