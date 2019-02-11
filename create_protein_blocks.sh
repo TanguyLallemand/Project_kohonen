44cc500c014e06fe681121ac633494e42491d4e8
tanguy.lallemand <tanguy.lallemand@gmail.com>
2019-02-11 14:50:24 +0100

    add comments

--- create_protein_blocks.sh
+++ create_protein_blocks.sh
@@ -4,6 +4,7 @@ mkdir -p ./protein_blocks
 for i in `ls *.ang`; do
     # Get name of PDB
     name=$(basename $i)
+    # remove extension from pdb filename
     name=$(echo "$name" | cut -f 1 -d '.')
     # Get number of commas to determine how many protein blocks we can do
     number_of_commas=`head -n1 $name.ang| tr -cd , | wc -c`
@@ -11,12 +12,15 @@ for i in `ls *.ang`; do
     # number_of_aa=`cat $name.seq | wc -c`
     # number_of_aa=$((number_of_aa-3))
 
-    # Change five by three because the two last Amino acids are missing with five. For example, for protein 1A3C, with j-5 last Amino acids are I, Y and E, N are missing. If we correct with 3 last amino acids are E and N.
+    # Change five by four because the two last Amino acids are missing with five. For example, for protein 1A3C, with j-5 last Amino acids are I, Y and E, N are missing. If we correct with 3 last amino acids are E and N.
     number_of_commas=$((number_of_commas-4))
-
+    # A loop to construct protein blocks
     for((num=1;num<=$number_of_commas;num++));do
+        # Get phi angle
         head -n1 $name.ang | cut -d ',' -f $num-$((num+4)) | paste -s -d ',' >> ./protein_blocks/$name.$num.ang
+        # Get psi angle
         tail -n1 $name.ang | cut -d ',' -f $num-$((num+4)) | paste -s -d ',' >> ./protein_blocks/$name.$num.ang
+        # Get sequence of protein block
         cat $name.seq | cut -c$num-$((num+4)) > ./protein_blocks/$name.$num.seq
     done
 done
