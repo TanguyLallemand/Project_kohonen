3b1e1816899d9a07d11da6e66a71f39a35d2dd87
TanguyLallemand <tanguy.lallemand@gmail.com>
2019-02-11 12:03:06 +0100

    seems to be optimized

--- create_protein_blocks.sh
+++ create_protein_blocks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf ./protein_blocks
 mkdir -p ./protein_blocks
 for i in `ls *.ang`; do
     # Get name of PDB
@@ -6,22 +7,16 @@ for i in `ls *.ang`; do
     name=$(echo "$name" | cut -f 1 -d '.')
     # Get number of commas to determine how many protein blocks we can do
     number_of_commas=`head -n1 $name.ang| tr -cd , | wc -c`
+    # Alternatively, it is possible to do following lines to get number of aas to determine how many protein blocks we can do
+    # number_of_aa=`cat $name.seq | wc -c`
+    # number_of_aa=$((number_of_aa-3))
+
     # Change five by three because the two last Amino acids are missing with five. For example, for protein 1A3C, with j-5 last Amino acids are I, Y and E, N are missing. If we correct with 3 last amino acids are E and N.
-    number_of_commas=$((number_of_commas-3))
-    echo $number_of_commas
+    number_of_commas=$((number_of_commas-4))
+
     for((num=1;num<=$number_of_commas;num++));do
-        head -n1 $name.ang| cut -d ',' -f $num-$((num+4)) | paste -s -d ',' >> ./protein_blocks/$name.$num.ang.csv
-        tail -n1 $name.ang| cut -d ',' -f $num-$((num+4)) | paste -s -d ',' >> ./protein_blocks/$name.$num.ang.csv
-    done
-
-
-    # Get number of commas to determine how many protein blocks we can do
-    number_of_aa=`cat $name.seq | wc -c`
-    echo $number_of_aa
-    # Change five by three because the two last Amino acids are missing with five. For example, for protein 1A3C, with j-5 last Amino acids are I, Y and E, N are missing. If we correct with 3 last amino acids are E and N.
-    number_of_aa=$((number_of_aa-3))
-    for((num=1;num<=$number_of_aa;num++));do
-        head -n1 $name.ang| cut -d ',' -f $num-$((num+4)) | paste -s -d ',' >> ./protein_blocks/$name.$num.seq.csv
-        tail -n1 $name.ang| cut -d ',' -f $num-$((num+4)) | paste -s -d ',' >> ./protein_blocks/$name.$num.seq.csv
+        head -n1 $name.ang | cut -d ',' -f $num-$((num+4)) | paste -s -d ',' >> ./protein_blocks/$name.$num.ang
+        tail -n1 $name.ang | cut -d ',' -f $num-$((num+4)) | paste -s -d ',' >> ./protein_blocks/$name.$num.ang
+        cat $name.seq | cut -c$num-$((num+4)) > ./protein_blocks/$name.$num.seq
     done
 done
