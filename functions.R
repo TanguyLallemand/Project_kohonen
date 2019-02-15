af4acfa79f486ba736d71d27858ccdfe211a483a
tanguy.lallemand <tanguy.lallemand@gmail.com>
2019-02-15 11:59:24 +0100

    progress on rmsd

---
+++ functions.R
@@ -0,0 +1,23 @@
+# Compute RMSD
+rmsd<-function(training_dataset_for_block_a, kohonen_matrix)
+{
+    difference<-as.numeric(training_dataset_for_block_a)-as.numeric(kohonen_matrix)
+    distance=sqrt(mean(difference^2))
+    distance=distance/ncol(training_dataset_for_block_a[1])
+    print(distance)
+    return(distance) 
+}
+
+#rmsd_result<-lapply(list_of_random_vector, rmsd_function, training_dataset_for_block_a=training_dataset_for_block_a[1,])
+# rmsd_function<-function(training_dataset_for_block_a, kohonen_matrix)
+# {
+#     rms<-0
+#     for (i in 1:ncol(training_dataset_for_block_a[1])) 
+#     {
+#         difference <- as.numeric(kohonen_matrix[i])-as.numeric(training_dataset_for_block_a[i])
+#         rms <- sqrt(mean(difference^2))
+#     }
+#     rms = rms/8
+#     print(rms)
+#     return(rms)
+# }
--- generate_random_vector_list.R
+++ generate_random_vector_list.R
@@ -1,40 +1,29 @@
-training_dataset_for_block_a <- read.csv("~/Documents/Master2/Semestre2/bioinfo_structurale/vetrivel/session3/Project_kohonen/dataset/training_dataset_for_block_a.phipsi", header=FALSE)
+source("functions.R")
+setwd(getwd())
+
+training_dataset_for_block_a <- read.csv("./dataset/training_dataset_for_block_a.phipsi", header=FALSE)
+#list_of_random_vector <- read.csv("~/Documents/Master2/Semestre2/bioinfo_structurale/vetrivel/session3/Project_kohonen/random_file.csv", header=FALSE)
 number_of_neurons<-16
 list_of_random_vector<-list()
 # A loop to generate a random dataset
 for (iterator in 1:number_of_neurons) 
 {
-  # Generate a vector of eight values
-  vector_rand <- round(runif(8, min=-180, max=180), digits=0)
-  # Save it in a list
-  list_of_random_vector[iterator] <- list(vector_rand)
+    # Generate a vector of eight values
+    vector_rand <- round(runif(8, min=-180, max=180), digits=0)
+    # Save it in a list
+    list_of_random_vector[iterator]<-list(vector_rand)
 }
+
+
 
 # Construction of a matrix for Kohonen map
 kohonen_matrix<-matrix(list_of_random_vector,ncol=sqrt(number_of_neurons))
-# Compute RMSD
-rmsd<-function(training_dataset_for_block_a, kohonen_matrix)
+
+
+for(x in 1:1)
 {
-    difference<-training_dataset_for_block_a-kohonen_matrix 
-    for(j in 1:length(difference))
-    {
-        if (difference[j]< -180) {difference[j]=difference[j]+360}
-        if (difference[j]> +180) {difference[j]=difference[j]-360}
-    }
-     distance=sqrt(mean(difference^2))
-     print(distance)
-     return(distance) 
+    rmsd_result<-lapply(list_of_random_vector, rmsd, training_dataset_for_block_a=training_dataset_for_block_a[1,])
 }
 
-for(x in 1:4)
-{
-    for (y in 1:4)
-    {
-        if(x==y)
-        {
-            break;
-        }
-        rmsd_test<-rmsd(as.numeric(training_dataset_for_block_a[x,]), as.numeric(vector_rand[x]))
-    }
-}
+
 
