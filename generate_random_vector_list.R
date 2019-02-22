b64c87651e0b2e7a23208006e49dadf33af2abdf
tanguy.lallemand <tanguy.lallemand@gmail.com>
2019-02-22 16:49:20 +0100

    add an argument parser!

--- generate_random_vector_list.R
+++ generate_random_vector_list.R
@@ -1,10 +1,39 @@
+#!/home/storm/anaconda3/envs/rstudio/bin/Rscript
+
 ###############################################################################
-# Load libraries, init some variables
+# Load libraries
 ###############################################################################
 # Library to build more beautiful graphs, if not installed please run install.packages("ggplot2")
 library("ggplot2")
 # Library to construct a multiple graph object, if not installed please run run install.packages("gridExtra")
 library("gridExtra")
+# Get argument using this parser if not installed please run run install.packages("optparse")
+library("optparse")
+
+###############################################################################
+# Setup argument parser
+###############################################################################
+option_list <- list(
+    make_option(c("-r", "--rate"), action="store_true", type="numeric", default=0.75,
+                help="give initial learn rate"),
+    make_option(c("-t", "--radius"), action="store_true", type="numeric", default=2.0,
+                help="give initial learn radius"),
+    make_option(c("-n", "--number_iteration"), action="store_true", type="integer", default=2, 
+                help="give number of iterations")
+); 
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+print(opt$rate)
+
+###############################################################################
+# Load functions, init some variables
+###############################################################################
+# Library to build more beautiful graphs, if not installed please run install.packages("ggplot2")
+library("ggplot2")
+# Library to construct a multiple graph object, if not installed please run run install.packages("gridExtra")
+library("gridExtra")
+# Set current path
 setwd(getwd())
 # Get functions from library file
 if(!exists("generate_a_random_dataset_function", mode="function")) source("./functions.R")
@@ -13,13 +42,13 @@ if(!exists("learning_function", mode="function")) source("./functions.R")
 if(!exists("generate_all_possible_combinations", mode="function")) source("./functions.R")
 if(!exists("construct_and_save_plots", mode="function")) source("./functions.R")
 
-# Configuration of parameters for algorithm
+# Configuration of parameters for algorithm, using passed informations by argument
 number_of_neurons<-16
-init_rate=0.75
-initial_radius=2
-number_max_iteration=3
-
-
+init_rate=opt$rate
+initial_radius=opt$radius
+number_max_iteration=opt$number_iteration
+# Construct name of output file using those variables
+paste(s1, s2, sep = "")
 ###############################################################################
 # Import datas, generate a random datatset
 ###############################################################################
@@ -49,7 +78,7 @@ for(current_iteration in 1:number_max_iteration)
         learn_rate<-learning_function(init_rate,((current_iteration-1)*nrow(training_dataset_for_block_a))+i_row,training_dataset_for_block_a)
         # Update learn_radius
         learn_radius<-learning_function(initial_radius,((current_iteration-1)*nrow(training_dataset_for_block_a))+i_row,training_dataset_for_block_a)
-        #Find distance between each vectors of angles of Kohonen Map and the training vector
+        # Find distance between each vectors of angles from Kohonen Map and current vector from the training dataset
         closest_cell <- rmsd_function(kohonen_matrix,training_dataset_for_block_a)
         # For every neurons, reconstruct a new matrix based on learning function
         for (mylist in 1:number_of_neurons)
