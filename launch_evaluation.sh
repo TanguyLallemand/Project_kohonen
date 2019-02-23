781f8751b7debe27aa97a21bdfc58255df4fd83d
TanguyLallemand <tanguy.lallemand@gmail.com>
2019-02-23 16:26:01 +0100

    add some results

--- launch_evaluation.sh
+++ launch_evaluation.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 ./self_organizing_map.R --rate 0.75 --radius 2 --number_iteration 1
+./self_organizing_map.R --rate 2 --radius 2 --number_iteration 1
+./self_organizing_map.R --rate 0.75 --radius 2 --number_iteration 3
+./self_organizing_map.R --rate 0.10 --radius 2 --number_iteration 1
+./self_organizing_map.R --rate 0.75 --radius 4 --number_iteration 1
+./self_organizing_map.R --rate 0.75 --radius 0.5 --number_iteration 1
+./self_organizing_map.R --rate 0.75 --radius 0.5 --number_iteration 3
+./self_organizing_map.R --rate 0.1 --radius 0.5 --number_iteration 1
+./self_organizing_map.R --rate 2 --radius 0.5 --number_iteration 3
+
